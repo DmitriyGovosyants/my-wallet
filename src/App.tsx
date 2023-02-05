c6f7fbc736f765c931d125f3f448a07052563a82
DmitriyGovosyants <Demiangvs@gmail.com>
2023-02-05 09:37:27 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
